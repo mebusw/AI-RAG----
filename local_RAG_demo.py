99211c675409ba343c6e64861f4f017bf93117e1
mebusw <mebusw@163.com>
2025-01-04 16:38:59 +0800

    feat: 预处理了文本段落，增加标记。假设预处理了PDF表格

--- local_RAG_demo.py
+++ local_RAG_demo.py
@@ -14,7 +14,65 @@ client = OpenAI(
     base_url = API_URL,
 )
 
-# Non-streaming:
+# Step 2.1 预处理文本段落：
+def read_text_file(file_name):
+    """
+    按行读取当前目录下的一个文本文件。
+
+    :param file_name: 文本文件名
+    :return: 包含文件内容的列表，每个元素为文件的一行
+    """
+    current_directory = os.getcwd()
+    file_path = os.path.join(current_directory, file_name)
+    
+    if not os.path.exists(file_path):
+        print(f"文件 {file_name} 不存在于目录 {current_directory}")
+        return []
+    
+    text_data = []
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text_data = file.readlines()
+    
+    return text_data
+
+file_name = "csm_course_outline.txt"  # 预处理过的文本
+doc_txt = read_text_file(file_name)
+# for line in doc_txt:
+#     print(line.strip())
+
+
+# 使用目录为段落添加标签
+title_map = { 
+    "1-10" : "Course title", 
+    "11-28" : "Company Introduction", 
+    "29-40" : "Course overview",
+    "41-48" : "Benefits to participants",
+    "49-58" : "Advantages of the course",
+    "59-75" : "Key Learning Objectives",
+    "76-81" : "Target participants",
+    "82-364" : "Agenda", 
+    "365-366" : "Attachements and Quotation", 
+}
+lst_docs, lst_ids, lst_metadata = [], [], []
+for n, page in enumerate(doc_txt):
+    try:
+        ## 获取标题
+        title = [v for k,v in title_map.items() if n in range(int(k.split("-")[0]), int(k.split("-")[1])+1)][0]
+        ## 清理页面
+        page = page.replace("Table of Contents","")
+        ## 获取段落
+        for i,p in enumerate(page.split('\n\n')):
+            if len(p.strip())>5: 
+                lst_docs.append(p.strip())
+                lst_ids.append(str(n)+"_"+str(i))
+                lst_metadata.append({"title":title})
+    except:
+        continue
+
+for doc, doc_id, metadata in zip(lst_docs, lst_ids, lst_metadata):
+    print(f"Document: {doc}, ID: {doc_id}, Metadata: {metadata}")
+
+# Step 2.2 预处理表格:
 table = """Table:
 Year Ended
 Jan 28,2024 Jan 29,2023 Change($ in millions, except per share data)
