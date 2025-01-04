0a60b2a3fee188d53bf191231a3a55a12220c33b
mebusw <mebusw@163.com>
2025-01-04 14:38:15 +0800

    feat: a working chat demo with Doubao API

---
+++ doubao_api_demo.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# Step 1: 加载环境变量
+load_dotenv()
+API_URL = os.getenv("DOUBAO_API_URL", "https://ark.cn-beijing.volces.com/api/v3")
+API_KEY = os.getenv("DOUBAO_API_KEY")
+
+client = OpenAI(
+    api_key = API_KEY,
+    base_url = API_URL,
+)
+
+# Non-streaming:
+print("----- standard request -----")
+completion = client.chat.completions.create(
+    model = "ep-20250103223903-7kzhd",  # your model endpoint ID
+    messages = [
+        {"role": "system", "content": "你是豆包，是由字节跳动开发的 AI 人工智能助手"},
+        {"role": "user", "content": "常见的十字花科植物有哪些？"},
+    ],
+)
+print(completion.choices[0].message.content)
+
+# Streaming:
+print("----- streaming request -----")
+stream = client.chat.completions.create(
+    model = "ep-20250103223903-7kzhd",  # your model endpoint ID
+    messages = [
+        {"role": "system", "content": "你是豆包，是由字节跳动开发的 AI 人工智能助手"},
+        {"role": "user", "content": "常见的十字花科植物有哪些？"},
+    ],
+    stream=True
+)
+
+for chunk in stream:
+    if not chunk.choices:
+        continue
+    print(chunk.choices[0].delta.content, end="")
+print()
